ddcd8188d738813002300a2e0701ec69ed12bf83
ahanlon <hanlonad27@gmail.com>
2015-06-30 15:31:36 -0600

    finished basic clock

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ $(document).on('ready', function() {
 	outerShell.append(innerShell);
 	$('.container').append(outerShell);
 
-	var leftAMPM = $('<p class="left-AMPM">AM/PM</p>');
+	var leftAM = $('<p class="left-AM">AM</p>');
+	var leftPM = $('<p class="left-PM">PM</p>');
 	var leftAuto = $('<p class="left-Auto">Auto</p>');
 	var clockScreen = $('<div class="clock-screen"></div>');
-	var bttmAMlabel = $('<div class="bttm-AM-lbl"></div>');
-	var bttmFMlabel = $('<div class="bttm-FM-lbl"></div>');
-	innerShell.append(leftAMPM).append(leftAuto).append(clockScreen).append(bttmAMlabel).append(bttmFMlabel);
+	var bttmAMlabel = $('<p class="bttm-am">AM 53 60 70 80 100 130 170 10kHz</p>');
+	var bttmFMlabel = $('<p class="bttm-fm">FM 88 92 96 100 104 108 MHz</p>');
+	innerShell.append(leftAM).append(leftPM).append(leftAuto).append(clockScreen).append(bttmAMlabel).append(bttmFMlabel);
 
 	var indicator = $('<div class="indicator"></div>');
-	var clkText = $('<div class="clk-text"></div>');
+	var clkText = $('<p class="clk-text">12:17</p>');
 	clockScreen.append(indicator).append(clkText);
 
 
